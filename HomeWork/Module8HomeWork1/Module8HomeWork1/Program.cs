8be731999c1a3fbffb53a46f9faccde4939d298f
oshuakbaev <olzhason47@gmail.com>
2023-10-27 09:42:54 +0600

    update

---
+++ HomeWork/Module8HomeWork1/Module8HomeWork1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Module8HomeWork
+{
+
+    // класс индексатора 
+    class Indexator
+    {
+        // создаем одномерный массив 
+        public int[] arr = new int[100];
+
+        public int this[int i]
+        {
+            get { return arr[i]; }
+
+            set { arr[i] = value * value; }
+
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            // здесь мы создаем объект класса индексатора и выводим. 
+
+            Indexator indexator = new Indexator();
+            indexator[0] = 5;
+
+            Console.WriteLine($"значение квадратного индексатора: {indexator[0]}");
+
+        }
+    }
+}
