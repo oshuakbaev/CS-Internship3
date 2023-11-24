25c186efb5e671757d104c50f6be46b98a732b6c
Олжас <olzas@iMac-Olzas.local>
2023-11-24 23:53:38 +0600

    update

---
+++ HomeWork/Module9HomeWork/Module9HomeWork/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Module9HomeWork
+{
+	public class Employee
+	{
+		public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, int age, int salary)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Salary = salary; 
+        }
+
+
+        public void GetInfo(string name) {
+
+            if(this.Name.Equals(name,StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Name: "+Name);
+                Console.WriteLine("Age: "+Age);
+                Console.WriteLine("Salary: "+Salary);
+
+            }
+            else
+            {
+                Console.WriteLine("this employee not found");
+            }
+            
+        }
+
+        public void CalculateAnnualSalary()
+        {
+            Console.WriteLine("Annual salary is : " + Salary * 12);
+        }
+
+
+    }
+}
+
